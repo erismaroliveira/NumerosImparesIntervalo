1920247d8de0a3bdd4f7fd36c833568dc016a4f1
Erismar Oliveira <74121763+erismaroliveira@users.noreply.github.com>
2022-09-15 11:07:16 -0300

    Initial commit

---
+++ NumerosImparesIntervalo/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+* Programa C# para imprimir números ímpares em um determinado intervalo
+*/
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NumerosImparesIntervalo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IEnumerable<int> numerosImpares = Enumerable.Range(20, 20).Where(x => x % 2 != 0);
+
+            foreach (var num in numerosImpares)
+            {
+                Console.WriteLine(num);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
